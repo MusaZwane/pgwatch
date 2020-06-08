5ea5880651fce1cb2456649944fb664d5e749d69
Musa Zwane - Technical Specialist C <f5288754@fnb.co.za>
2020-06-08 15:17:03 +0200

    Modified the script to change the perms on the added dirs

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 docker-compose down
-chmod 777 grafana
-chmod 777 influx && chmod -R 777 logs/ && chmod 777 pg && chmod 777 pw2
+
+
+for i in grafana influx logs/ pg pw2;
+do
+	chmod -R 777 $i
+done
+
 docker-compose up -d --force-recreate
